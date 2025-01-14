d4a66232c3bfe8abc99cab59f00cbd5d8b5fcd92
left2east <left2east@163.com>
2025-01-14 19:56:54 +0800

    TestController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,12 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class TestController extends Controller
 {
-    public function index()
+    public function testdb()
     {
         $users = DB::select('select * from user where role = ?', [2]);
         return view('test', ['users' => $users]);
+    }
+
+    public function index()
+    {
+        $responses = Http::local()->get('/test/first');
+        
+        return [
+            'first' => $responses->body(),
+        ];
+    }
+
+    public function first()
+    {
+        return 'This is the first response';
+    }
+
+    public function second()
+    {
+        return 'This is the second response';
+    }
+
+    public function third()
+    {
+        return 'This is the third response';
     }
 }
